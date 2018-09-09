1883247efb147b7373d21b266a913f43b3c6d397
David <davidchincharashvili@gmail.com>
2018-09-09 23:42:31 +0400

    initialization

---
+++ index.php
@@ -0,0 +1,135 @@
+<?php header( "Cache-Control: no-cache, must-revalidate" ); ?>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<button>upload</button>
+
+<?php
+require 'vendor/autoload.php';
+// functions
+function pp( $data ) {
+	echo '<pre>';
+	print_r( $data );
+	echo '</pre>';
+}
+define( "BR", "<br />" );
+function cl( $data ) {
+	echo '<script>';
+	echo 'console.log(' . json_encode( $data, JSON_FORCE_OBJECT ) . ')';
+	echo '</script>';
+}
+$pdo = new PDO("mysql:dbname=bible;charset=utf8", "root");
+$db = new FluentPDO($pdo);
+
+$q = $db->from('ortodox')->orderBy('id desc')->limit(1);
+echo 'ბოლო ატვირთული: ';
+
+
+/************/
+// change after uploading one book
+$ch  = 1;
+$wigni = 'osia';
+$auto = true;
+/************/
+?>
+<?php
+
+ foreach ($q as $a):
+    print_r($wigni.' '.$a['chapter_id'].':'.$a['verse_id']
+            . ' <p style="color:darkslategray"> ' .$a['text'] ."</p>"
+    );
+
+// ****** auto next Chapters ******
+ if ($auto) :
+ $ch = ++$a['chapter_id']; $book = $a['book_id'];
+ else:
+// ************
+	 // first chapter (inc book)
+     $book = ++$a['book_id'];
+ endif;
+ endforeach;
+
+$client = new GuzzleHttp\Client();
+
+$url = 'http://www.orthodoxy.ge/tserili/biblia_sruli/dzveli/'.$wigni.'/'.$wigni.'-' . $ch . '.htm';
+echo BR,BR,BR,BR,"<a href='$url' target='_blank'>ბიბლია</a>";
+try{
+$res = $client->request( 'GET', $url);
+echo $res->getBody();
+}
+catch (Exception $e){
+	echo "<a href='http://www.orthodoxy.ge/tserili/biblia_sruli/sarchevi.php'> სარჩევი </a>";
+	echo BR.'<h2>ასეთი გვერდი არ არსებობს</h2> ' . $e ;
+    die;
+}
+?>
+
+
+<script>
+    console.clear();
+    function myTrim(x) {
+        return x.replace(/^\s+|\s+|\n+$/gm,' ');
+    }
+
+
+
+    console.log('\n\n\n');
+    var json =[];
+    var table = document.querySelectorAll('table')[2];
+    var allRows = table.querySelectorAll('tr');
+
+    allRows.forEach(function (element, index) {
+        var obj = {};
+        element.querySelectorAll('td').forEach(function (td, count) {
+
+            switch (count) {
+                case 0:
+                    obj.id = parseInt(td.innerHTML);
+                    break;
+                case 1:
+                    obj.book = <?php echo $book ?>;
+                    obj.chapter = <?php echo $ch ?>;
+                    obj.text = myTrim(td.innerHTML);
+                    json.push(obj);
+                    break;
+
+            }
+        });
+    });
+
+function parseToPost (obj) {
+    var keys = Object.keys(obj);
+    var str= '';
+    keys.forEach(function (name,value) {
+        str += name+'='+obj[name] +"&";
+    });
+    str = str.substr(0,str.length -1);
+    return str;
+}
+function post(array,obj) {
+    axios({
+        method: 'post',
+        url:'http://localhost/guzzle/post.php',
+        data: parseToPost(array[obj]),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    })
+        .then(function (res) {
+            if (obj >= array.length-1) {
+//                return;
+                 location.reload();
+            }
+            post(array,obj+1);
+        })
+        .catch(function(e){
+            console.log(e)
+        });
+}
+function upload(e) {
+    post(json,0);
+    document.querySelector('button').style.display = 'none';
+}
+</script>
+
+<?php if ($auto) : ?>
+<script>document.querySelector('button').addEventListener('click', upload() );</script>
+<?php else: ?>
+<script>document.querySelector('button').addEventListener('click', upload );</script>
+<?php endif ?>
---
+++ post.php
@@ -0,0 +1,50 @@
+<?php header("Cache-Control: no-cache, must-revalidate");?>
+
+	<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+		      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Document</title>
+		<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+	</head>
+	<body>
+	<script>
+        //http://holybible.ge/breadphp.php
+        axios.get('http://www.orthodoxy.ge/tserili/biblia_sruli/dzveli/dabadeba/dabadeba-1.htm')
+	        .then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error)
+        });
+
+
+	</script>
+	</body>
+	</html>
+
+<?php
+
+require 'vendor/autoload.php';
+
+
+$pdo = new PDO("mysql:dbname=bible;charset=utf8", "root");
+$db = new FluentPDO($pdo);
+
+
+if(isset($_POST['book'])) {
+    $p = $_POST;
+    $values =
+        [
+            'id' => null,
+            'verse_id' => $p['id'],
+            'chapter_id' => $p['chapter'],
+            'book_id' => $p['book'],
+            'text' => $p['text']
+        ];
+ $db->insertInto('ortodox',$values)->execute();
+
+}
+
+?>
